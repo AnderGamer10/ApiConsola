59c7954efed29d3109b4c7eb76cbd3aa433aa871
AnderGamer10 <anderedondo10@hotmail.com>
2022-01-23 13:01:24 +0100

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/InformacionTiempo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class InformacionTiempo
+    {
+        public string Municipio { get; set; }
+        public string Region { get; set; }
+        public string Datos_ultima_hora { get; set; }
+        public string Temperatura { get; set; }
+        public string Humedad { get; set; }
+        public string Velocidad_Viento { get; set; }
+        public string Precipitacion_Acumulada { get; set; }
+        public string GpxX { get; set; }
+        public string GpxY { get; set; }
+        
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using NW = Newtonsoft.Json;
+using MS = System.Text.Json;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+//Obtenemos las estaciones
+//https://www.euskalmet.euskadi.eus/vamet/stations/stationList/stationList.json
+//Obtenemos los datos de las estaciones
+//https://www.euskalmet.euskadi.eus/vamet/stations/readings/C00B/2022/01/22/readingsData.json
+
+var requestUrl = $"https://www.euskalmet.euskadi.eus/vamet/stations/stationList/stationList.json";
+
+Debug.WriteLine(requestUrl);
+var client = new HttpClient { BaseAddress = new Uri(requestUrl) };
+var responseMessage = await client.GetAsync("", HttpCompletionOption.ResponseContentRead);
+var resultData = await responseMessage.Content.ReadAsStringAsync();
+dynamic stationTypeJson = JsonConvert.DeserializeObject(resultData);
+foreach(var items in stationTypeJson)
+{
+    Console.WriteLine(items.id);
+    var cliente = new HttpClient { BaseAddress = new Uri($"https://www.euskalmet.euskadi.eus/vamet/stations/readings/{items.id}/2022/01/22/readingsData.json")};
+    var responseMessagee = await cliente.GetAsync("", HttpCompletionOption.ResponseContentRead);
+    var resultDatae = await responseMessagee.Content.ReadAsStringAsync();
+    dynamic stationReadingsJson = JsonConvert.DeserializeObject(resultDatae);
+    foreach (var items2 in stationReadingsJson)
+    {
+        foreach(JObject item in items2)
+        {
+            String dataType = item["name"].ToString();
+            JObject preDataJson = JObject.Parse(item["data"].ToString());
+            IList<string> keys = preDataJson.Properties().Select(p => p.Name).ToList();
+            JObject dataJson = JObject.Parse(preDataJson[keys[0]].ToString());
+            switch (dataType)
+            {
+                case "temperature":
+                    Console.WriteLine(dataJson.ToString());
+                    List<string> dataJsonTimeList = dataJson.Properties().Select(p => p.Name).ToList();
+                    dataJsonTimeList.Sort();
+                    //Console.WriteLine(dataJsonTimeList.Last());
+                    break;
+            }
+        }
+        
+    }
+}
+
+/*
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+
+}
+
+*/
